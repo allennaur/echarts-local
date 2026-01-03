ee4b59041f642a0e1321ab3c7d891be65af930c7
Allen-imac <itman003@icloud.com>
2026-01-03 13:28:19 +0800

    build: 配置 GitHub Pages 基础路径并改用 HashRouter
    将 vite.config.ts 中的 base 路径设置为 '/magic-charts/' 以适配 GitHub Pages
    将 BrowserRouter 替换为 HashRouter 以解决 GitHub Pages 路由问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '@/pages/Home';
 import ChartDetail from '@/pages/ChartDetail';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chart/:id" element={<ChartDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
